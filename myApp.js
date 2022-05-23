46a7b35ffc700e245be7e1dbd140cdc1697563c3
dkkret <dk.kret@gmail.com>
2022-05-23 22:43:52 +0200

    fix sort

--- myApp.js
+++ myApp.js
@@ -109,7 +109,7 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-  Person.find({favoriteFoods: foodToSearch}).sort({name: asc}).limit(2).select('name favoriteFoods').exec((err, data) => {
+  Person.find({favoriteFoods: foodToSearch}).sort({name: 'asc'}).limit(2).select('name favoriteFoods').exec((err, data) => {
     if( err) return done(err)
     done(null, data)
   })
