94b1c258d144d79dbb8d4311bad364a467e66f71
dkkret <dk.kret@gmail.com>
2022-05-23 21:26:24 +0200

    findAndUpdate

--- myApp.js
+++ myApp.js
@@ -80,8 +80,13 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, data) => {
+    if(err) {
+      return done(err)
+    } else {
+      done(null, data)
+    }
+  })
 };
 
 const removeById = (personId, done) => {
